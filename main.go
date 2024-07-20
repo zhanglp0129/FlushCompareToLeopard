86b9f2752c2243c4ce254c9e0ebeb8e0501c4318
zhanglp0129 <zhanglp0129@gmail.com>
2024-07-20 16:06:19 +0800

    实现了并发模拟

--- main.go
+++ main.go
@@ -1,17 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+var (
+	flush   = 0
+	leopard = 0
+	mutex   = sync.Mutex{}
+	wait    = sync.WaitGroup{}
+)
 
 func main() {
-	count := 1000000000
-	flush, leopard := run(count)
+	var count int
+	fmt.Print("请输入模拟次数(单位为万)：")
+	for {
+		_, err := fmt.Scan(&count)
+		if err != nil {
+			fmt.Println("输入错误，请重新输入！")
+		} else {
+			break
+		}
+	}
+	count *= 10000
+
+	// 创建100个协程，来模拟
+	goroutineCount := 100
+	wait.Add(goroutineCount)
+	for i := 0; i < goroutineCount; i++ {
+		go run(count / goroutineCount)
+	}
+	wait.Wait()
+
+	// 模拟完成，输出结果
 	fmt.Printf("同花顺：%.4f%%\n", float64(flush)/float64(count)*100)
 	fmt.Printf("豹子：%.4f%%\n", float64(leopard)/float64(count)*100)
+
+	// 暂停终端
+	fmt.Print("\n按回车键退出...")
+	fmt.Scanln()
 }
 
 // 运行模拟。需传入模拟次数，分别返回同花顺和豹子的次数
-func run(count int) (int, int) {
-	flush, leopard := 0, 0
+func run(count int) {
+	tempFlush, tempLeopard := 0, 0
 	for i := 0; i < count; i++ {
 		t, err := GetPokerType(PokerDeal())
 		if err != nil {
@@ -20,10 +53,16 @@ func run(count int) (int, int) {
 
 		switch t {
 		case StraightFlush:
-			flush++
+			tempFlush++
 		case Leopard:
-			leopard++
+			tempLeopard++
 		}
 	}
-	return flush, leopard
+
+	// 添加总结果数
+	mutex.Lock()
+	flush += tempFlush
+	leopard += tempLeopard
+	mutex.Unlock()
+	wait.Done()
 }
